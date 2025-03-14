deb5944ef8b5d0aa98f51d5829c1f0a049a3af09
JusteFancarron <68083701+JusteFancarron@users.noreply.github.com>
2025-03-14 19:41:12 +0100

    Refactorisation + Fix d'un problème dans plateaux.py
    J'ai rendu le code plus joli à lire et réglé un problème de double nom dans la classe "Ship". "placement" était utilisé à la fois comme un attribut de l'objet et à la fois comme une fonction, ce qui causait un conflit de noms.

--- plateaux.py
+++ plateaux.py
@@ -6,37 +6,52 @@ class Plateau:
         self.__sizex=sizex
         self.__sizey=sizey
         self.__flottemanquante={2:1,3:2,4:1,5:1} #size:amount
+
     def is_ready(self):
         return all(self.__flottemanquante.values()==0)
+
     def place_ship(self,emplacements:list[tuple[int,int]]):
         if not(2<=len(emplacements)<=5) or any(len(emplacements[i])!=2 for i in range(len(emplacements))):
             raise ValueError("Unproper format for emplacements")
+
         if self.__flottemanquante[len(emplacements)]==0:
             raise ValueError("No ships of this size are available")
-        if any(self.__grid[i].placement[j]==emplacements[k] for i in range(len(self.__grid)) for j in range(self.__grid[i].placement) for k in range(len(emplacements))):
+
+        if any(self.__grid[i].placement[j]==emplacements[k]
+                for i in range(len(self.__grid))
+                for j in range(self.__grid[i].placement)
+                for k in range(len(emplacements))):
             raise ValueError("Place already occupied")
+
         for i in range(len(emplacements)):
             if not(not(0<=emplacements[i][0]<=self.__sizey and 0<=emplacements[i][1]<=self.__sizex)):
                 raise ValueError(f"Tried to place a ship at {emplacements[i]} even though this place isn't on the board")
+
         new=Ship()
         new.place(emplacements)
         self.__grid.append(new)
 
-
 class Ship:
     def __init__(self):
-        self.placement=None
-    def place(self,places:list[list[int,int]]):
-        self.placement=places
+        self._placement = None
+
+    def place(self, places: list[list[int, int]]):
+        self._placement = places
+
     @property
     def placement(self):
-        return self.placement
+                return self._placement
+
+    @placement.setter
+    def placement(self, value):
+        self._placement = value
+
     @property
     def size(self):
-        return len(self.placement)
-    
-test=Ship()
-test.placement=5
+        return len(self._placement) if self._placement else 0
+
+test = Ship()
+test.placement = 5
 """
 1=j
 2=d
