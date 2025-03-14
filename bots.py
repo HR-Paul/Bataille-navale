1e95310b901ff7985f5245744ba6ec482ad4c357
Paul Le Dévédec <paul.le-devedec@epita.fr>
2025-03-14 15:10:36 +0100

    Initializing the file bots.py

---
+++ bots.py
@@ -0,0 +1,8 @@
+from plateaux import Plateau
+def facile(grid:Plateau)->list[int,int]:
+    pass
+def moyen(grid:Plateau)->list[int,int]:
+    pass
+def difficile(grid:Plateau)->list[int,int]:
+    pass
+difficultes={"facile":facile,"moyen":moyen,"difficile":difficile}
